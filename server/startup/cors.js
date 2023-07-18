0161876d0a060ced25a6e09ae4a4b9411fa4329b
Ahmad Fardous Azimi <ahmadazimi@Ahmads-MacBook-Air.local>
2023-07-18 16:47:39 +0300

    Added vercel.json

--- server/startup/cors.js
+++ server/startup/cors.js
@@ -1,5 +1,9 @@
 const cors = require("cors");
 
 module.exports = function (app) {
-    app.use(cors());
+    app.use(cors({
+        origin: ["https://vidly-api-rho.vercel.app/"],
+        methods: ['POST', "GET", "DELETE", "PUT"],
+        credentials: true
+    }));
 };
