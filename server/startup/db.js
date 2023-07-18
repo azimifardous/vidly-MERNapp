11b879b24712cf569d7d5be223da235e0db8427e
Ahmad Fardous Azimi <ahmadazimi@Ahmads-MacBook-Air.local>
2023-07-18 15:00:54 +0300

    Added db

--- server/startup/db.js
+++ server/startup/db.js
@@ -3,7 +3,7 @@ const logger = require('./logger')();
 const config = require('config');
 
 module.exports = function () {
-    const db = "mongodb + srv://vidly:vidly23@cluster0.oigid09.mongodb.net/?retryWrites=true&w=majority";
+    const db = config.get('db');
     // connecting to the db
     mongoose.connect(db)
         .then(() => logger.info(`Connected to ${db}...`));
